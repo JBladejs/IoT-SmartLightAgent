9bc31407c947faff41c05b0e61f071cb69aaba6d
JBladejs <JBladejs@gmail.com>
2020-05-19 00:26:51 +0200

    Implemented basic device connection

--- src/main/java/com/github/jbladejs/iot/StreetLight.kt
+++ src/main/java/com/github/jbladejs/iot/StreetLight.kt
@@ -1,5 +1,22 @@
 package com.github.jbladejs.iot
 
-class StreetLight(private val connectionString: String) {
+import com.microsoft.azure.sdk.iot.device.DeviceClient
+import com.microsoft.azure.sdk.iot.device.IotHubClientProtocol
+import java.lang.Exception
 
+class StreetLight(private val connectionString: String) {
+    private var client = DeviceClient(connectionString, IotHubClientProtocol.MQTT)
+
+    init{
+        println("Starting device...")
+        try {
+            client.open()
+        } catch (ex: Exception) {
+            error("Error when starting device!")
+            ex.printStackTrace()
+        }
+        println("Device successfully started!")
+    }
+
+    fun closeConnection() = client.closeNow()
 }
--- src/test/java/com/github/jbladejs/iot/Test.kt
+++ src/test/java/com/github/jbladejs/iot/Test.kt
@@ -1,4 +1,5 @@
 package com.github.jbladejs.iot
 
 fun main() {
+    val light = StreetLight(FileHandler.getLinesArrayFromFile("devices_connection_strings.txt")[0])
 }
