3a17d263b37abb0a96b032243de0c06183324cbd
JBladejs <JBladejs@gmail.com>
2020-05-19 19:39:33 +0200

    Implemented TestDriver methods

--- src/main/java/com/github/jbladejs/iot/Driver.kt
+++ src/main/java/com/github/jbladejs/iot/Driver.kt
@@ -3,7 +3,7 @@ package com.github.jbladejs.iot
 interface Driver {
     fun getLightIntensity() : Int
     fun getEnergyUsage() : Int
+    fun isLightOn() : Boolean
     fun turnOnTheLight()
     fun turnOffTheLight()
-    fun isLightOn() : Boolean
 }
--- src/test/java/com/github/jbladejs/iot/TestDriver.kt
+++ src/test/java/com/github/jbladejs/iot/TestDriver.kt
@@ -1,23 +1,21 @@
 package com.github.jbladejs.iot
 
-class TestDriver : Driver {
-    override fun getLightIntensity(): Int {
-        TODO("Not yet implemented")
-    }
+import kotlin.random.Random
 
-    override fun getEnergyUsage(): Int {
-        TODO("Not yet implemented")
-    }
+class TestDriver : Driver {
+    private var light: Boolean = false
+
+    override fun getLightIntensity(): Int = Random.nextInt(50,60)
+
+    override fun getEnergyUsage(): Int = Random.nextInt(0,200)
+
+    override fun isLightOn(): Boolean = light
 
     override fun turnOnTheLight() {
-        TODO("Not yet implemented")
+        light = true
     }
 
     override fun turnOffTheLight() {
-        TODO("Not yet implemented")
-    }
-
-    override fun isLightOn(): Boolean {
-        TODO("Not yet implemented")
+        light = false
     }
 }
